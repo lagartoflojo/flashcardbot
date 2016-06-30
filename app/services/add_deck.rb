4f3f2869bf25a95dddcc1400883a2ab22a1db2e9
Hernán Schmidt <lagartoflojo@gmail.com>
2016-06-30 13:39:08 +0200

    Change user's status to adding_deck when issuing /newdeck command

--- app/services/add_deck.rb
+++ app/services/add_deck.rb
@@ -8,6 +8,7 @@ class AddDeck
   def call
     chat_id = update.message.chat.id
     client.api.send_message(chat_id: chat_id, text: "What’s the name of this deck?")
+    user.adding_deck!
   end
 
   private
--- spec/services/add_deck_spec.rb
+++ spec/services/add_deck_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe AddDeck do
   let(:user) { User.create telegram_id: 123123, first_name: 'Bob' }
   let(:update_hash) {
     {:update_id=>321321,
-      :message=>
+      update_type =>
       {:message_id=>38,
         :from=>{:id=>1, :first_name=>"Hernan", :last_name=>"Schmidt", :username=>"hernan"},
         :chat=>{:id=>1, :first_name=>"Hernan", :last_name=>"Schmidt", :username=>"hernan", :type=>"private"},
@@ -10,22 +10,37 @@ RSpec.describe AddDeck do
         :text=>"/newdeck",
         :entities=>[{:type=>"bot_command", :offset=>0, :length=>8}]}}
   }
+  let(:update_type) { :message }
   let(:update) { Telegram::Bot::Types::Update.new update_hash }
   let(:service) { AddDeck.new(user, update) }
 
   let(:api_response) {
-    {:message_id=>38,
-      :from=>{:id=>1, :first_name=>"Hernan", :last_name=>"Schmidt", :username=>"hernan"},
+    {:message_id=>39,
+      :from=>{:id=>1, :first_name=>"Hippocardus", :username=>"hippocardus"},
       :chat=>{:id=>1, :first_name=>"Hernan", :last_name=>"Schmidt", :username=>"hernan", :type=>"private"},
       :date=>1467277868,
       :text=>"/newdeck",
       :entities=>[{:type=>"bot_command", :offset=>0, :length=>8}]}.to_json
   }
 
-  it 'asks the user for the name of the deck' do
-    stub_request(:post, "https://api.telegram.org/bot#{Rails.application.secrets.bot_token}/sendMessage").
+  context 'when receiving the /newdeck command' do
+    before do
+      stub_request(:post, "https://api.telegram.org/bot#{Rails.application.secrets.bot_token}/sendMessage").
       with(:body => {"chat_id"=>"1", "text"=>"What’s the name of this deck?"}).
       and_return(status: 200, body: api_response)
-    service.call
+    end
+
+    it 'asks the user for the name of the deck' do
+      service.call
+    end
+
+    it 'changes the user status to `adding_deck`' do
+      service.call
+      expect(user).to be_adding_deck
+    end
+  end
+
+  context 'the update is not a message' do
+    it 'asks for the name again'
   end
 end
